0d0911d0029fb5575c5f5a91d7c6db9b45960040
Enebres <143394767+Elbert-Nebres@users.noreply.github.com>
2023-10-27 17:16:21 +0800

    Add files via upload

--- db_nt3102.sql
+++ db_nt3102.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Oct 22, 2023 at 11:45 PM
--- Server version: 8.0.18
--- PHP Version: 7.3.12
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 27, 2023 at 09:13 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,12 +24,111 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `adminID` int NOT NULL AUTO_INCREMENT,
+  `adminFN` text,
+  `adminLN` text,
+  `adminAddress` text,
+  `adminPH` text,
+  `salt` text,
+  PRIMARY KEY (`adminID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+CREATE TABLE IF NOT EXISTS `author` (
+  `authorID` int NOT NULL AUTO_INCREMENT,
+  `authorName` text,
+  PRIMARY KEY (`authorID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE IF NOT EXISTS `book` (
+  `bookID` int NOT NULL AUTO_INCREMENT,
+  `authorID` int DEFAULT NULL,
+  `publisherID` int DEFAULT NULL,
+  `bookTitle` text,
+  `ISBN` text,
+  `description` longtext,
+  `genre` text,
+  `publishDate` date DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  PRIMARY KEY (`bookID`),
+  KEY `authorID` (`authorID`),
+  KEY `publisherID` (`publisherID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookcondition`
+--
+
+DROP TABLE IF EXISTS `bookcondition`;
+CREATE TABLE IF NOT EXISTS `bookcondition` (
+  `statusID` int NOT NULL AUTO_INCREMENT,
+  `State` text,
+  `bookID` int DEFAULT NULL,
+  `count` int DEFAULT NULL,
+  PRIMARY KEY (`statusID`),
+  KEY `bookID` (`bookID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `librarian`
+--
+
+DROP TABLE IF EXISTS `librarian`;
+CREATE TABLE IF NOT EXISTS `librarian` (
+  `libID` int NOT NULL AUTO_INCREMENT,
+  `libFN` text,
+  `libLN` text,
+  `librarianAddress` text,
+  `librarianPH` text,
+  `salt` text,
+  PRIMARY KEY (`libID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publisher`
+--
+
+DROP TABLE IF EXISTS `publisher`;
+CREATE TABLE IF NOT EXISTS `publisher` (
+  `publisherID` int NOT NULL AUTO_INCREMENT,
+  `publisherName` text,
+  `publisherAddress` text,
+  PRIMARY KEY (`publisherID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbemployee`
 --
 
 DROP TABLE IF EXISTS `tbemployee`;
 CREATE TABLE IF NOT EXISTS `tbemployee` (
-  `empid` int(11) NOT NULL,
+  `empid` int NOT NULL,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `department` varchar(20) NOT NULL
@@ -44,7 +142,7 @@ CREATE TABLE IF NOT EXISTS `tbemployee` (
 
 DROP TABLE IF EXISTS `tb_studentinfo`;
 CREATE TABLE IF NOT EXISTS `tb_studentinfo` (
-  `studid` int(11) NOT NULL,
+  `studid` int NOT NULL,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL
@@ -56,6 +154,39 @@ CREATE TABLE IF NOT EXISTS `tb_studentinfo` (
 
 INSERT INTO `tb_studentinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
 (1, 'parker', 'peter', 'bsit');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `updatelog`
+--
+
+DROP TABLE IF EXISTS `updatelog`;
+CREATE TABLE IF NOT EXISTS `updatelog` (
+  `updateID` int NOT NULL AUTO_INCREMENT,
+  `activity` text,
+  `entityType` text,
+  `entityID` int DEFAULT NULL,
+  `printDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`updateID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertbl`
+--
+
+DROP TABLE IF EXISTS `usertbl`;
+CREATE TABLE IF NOT EXISTS `usertbl` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `userFN` text,
+  `userLN` text,
+  `userAddress` text,
+  `userPH` text,
+  `salt` text,
+  PRIMARY KEY (`userID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
