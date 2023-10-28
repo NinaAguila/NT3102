29e58aea5ec3bf01bcc83b042c7ec45bc6ba9a45
Yvan Anuyo <143691901+Yvan39@users.noreply.github.com>
2023-10-28 09:43:02 +0800

    Add files via upload

--- db_nt3102.sql
+++ db_nt3102.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Oct 22, 2023 at 11:45 PM
--- Server version: 8.0.18
--- PHP Version: 7.3.12
+-- Host: 127.0.0.1
+-- Generation Time: Oct 28, 2023 at 03:27 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,43 +18,180 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_nt3102`
+-- Database: `inventory_system`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbemployee`
+-- Table structure for table `add_stocks`
 --
 
-DROP TABLE IF EXISTS `tbemployee`;
-CREATE TABLE IF NOT EXISTS `tbemployee` (
-  `empid` int(11) NOT NULL,
-  `lastname` varchar(25) NOT NULL,
-  `firstname` varchar(25) NOT NULL,
-  `department` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `add_stocks` (
+  `Product_ID` int(100) NOT NULL,
+  `Product_Name` varchar(100) NOT NULL,
+  `Description` varchar(100) NOT NULL,
+  `Price` int(50) NOT NULL,
+  `Quantity` int(50) NOT NULL,
+  `Transaction_No` int(50) NOT NULL,
+  `Employee_Name` varchar(10) NOT NULL,
+  `Date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_studentinfo`
+-- Table structure for table `category`
 --
 
-DROP TABLE IF EXISTS `tb_studentinfo`;
-CREATE TABLE IF NOT EXISTS `tb_studentinfo` (
-  `studid` int(11) NOT NULL,
-  `lastname` varchar(25) NOT NULL,
-  `firstname` varchar(25) NOT NULL,
-  `course` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `category` (
+  `Category_ID` int(100) NOT NULL,
+  `Category_Name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `tb_studentinfo`
+-- Table structure for table `out_stocks`
 --
 
-INSERT INTO `tb_studentinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(1, 'parker', 'peter', 'bsit');
+CREATE TABLE `out_stocks` (
+  `Product_ID` int(100) NOT NULL,
+  `Stocks` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `Product_ID` int(100) NOT NULL,
+  `Product_Name` varchar(100) DEFAULT NULL,
+  `Product_Description` varchar(100) DEFAULT NULL,
+  `Product_Price` int(50) NOT NULL,
+  `Category_ID` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_data`
+--
+
+CREATE TABLE `user_data` (
+  `User_ID` int(255) NOT NULL,
+  `User_Code` varchar(100) DEFAULT NULL,
+  `Last_Name` varchar(100) DEFAULT NULL,
+  `First_Name` varchar(100) DEFAULT NULL,
+  `User_Type` varchar(100) DEFAULT NULL,
+  `Classification` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_login`
+--
+
+CREATE TABLE `user_login` (
+  `User_ID` int(255) NOT NULL,
+  `User_Type` varchar(100) DEFAULT NULL,
+  `User_Email` varchar(100) DEFAULT NULL,
+  `User_Password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `add_stocks`
+--
+ALTER TABLE `add_stocks`
+  ADD PRIMARY KEY (`Product_ID`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`Category_ID`);
+
+--
+-- Indexes for table `out_stocks`
+--
+ALTER TABLE `out_stocks`
+  ADD PRIMARY KEY (`Product_ID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`Product_ID`),
+  ADD KEY `Category_ID` (`Category_ID`);
+
+--
+-- Indexes for table `user_data`
+--
+ALTER TABLE `user_data`
+  ADD PRIMARY KEY (`User_ID`);
+
+--
+-- Indexes for table `user_login`
+--
+ALTER TABLE `user_login`
+  ADD PRIMARY KEY (`User_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `add_stocks`
+--
+ALTER TABLE `add_stocks`
+  MODIFY `Product_ID` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `Category_ID` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `out_stocks`
+--
+ALTER TABLE `out_stocks`
+  MODIFY `Product_ID` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `Product_ID` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_data`
+--
+ALTER TABLE `user_data`
+  MODIFY `User_ID` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_login`
+--
+ALTER TABLE `user_login`
+  MODIFY `User_ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`Category_ID`) REFERENCES `category` (`Category_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
