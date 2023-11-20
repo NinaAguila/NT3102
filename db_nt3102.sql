9468804d89a118fcb6e3941b79ed07ce8445c8c6
Fryan Auric L. Valdez <145759056+AuricValdez@users.noreply.github.com>
2023-11-20 08:30:06 +0800

    Add files via upload

--- db_nt3102.sql
+++ db_nt3102.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Oct 22, 2023 at 11:45 PM
--- Server version: 8.0.18
--- PHP Version: 7.3.12
+-- Host: 127.0.0.1
+-- Generation Time: Nov 20, 2023 at 01:15 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,16 +24,91 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `attendance_tbl`
+--
+
+CREATE TABLE `attendance_tbl` (
+  `AttendanceID` int(11) NOT NULL,
+  `FacultyID` varchar(25) NOT NULL,
+  `StudentID` varchar(25) NOT NULL,
+  `AttendanceDate` varchar(25) NOT NULL,
+  `ClassSection` varchar(25) NOT NULL,
+  `Room` varchar(25) NOT NULL,
+  `TimeStart` time(6) NOT NULL,
+  `TimeEnd` time(6) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `attendance_tbl`
+--
+
+INSERT INTO `attendance_tbl` (`AttendanceID`, `FacultyID`, `StudentID`, `AttendanceDate`, `ClassSection`, `Room`, `TimeStart`, `TimeEnd`) VALUES
+(1, '1', '3', '2023-11-02', 'BSIT-NT-3102', 'CECS 503', '08:00:00.000000', '11:00:00.000000'),
+(2, '1', '2', '2023-11-02', 'BSIT-NT-3102', 'CECS 503', '08:00:00.000000', '11:00:00.000000'),
+(3, '1', '1', '2023-11-02', 'BSIT-NT-3102', 'CECS 503', '08:00:00.000000', '11:00:00.000000'),
+(4, '1', '3', '2023-11-04', 'BSIT-NT-3102', 'HEB 502', '12:00:00.000000', '16:00:00.000000'),
+(11, '1', '1', '2023-11-17', 'BSIT-NT-1104', 'CECS 301', '08:00:00.000000', '12:00:00.000000'),
+(10, '1', '3', '2023-11-17', 'BSCS-NT-1102', 'HEB 402', '12:00:00.000000', '16:00:00.000000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty_tbl`
+--
+
+CREATE TABLE `faculty_tbl` (
+  `FacultyID` int(11) NOT NULL,
+  `empid` int(11) NOT NULL,
+  `FacultyPassword` varchar(25) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `faculty_tbl`
+--
+
+INSERT INTO `faculty_tbl` (`FacultyID`, `empid`, `FacultyPassword`) VALUES
+(1, 1, 'jenniferreyes123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_tbl`
+--
+
+CREATE TABLE `student_tbl` (
+  `StudentID` int(11) NOT NULL,
+  `studid` int(11) NOT NULL,
+  `StudentQR` varchar(300) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `student_tbl`
+--
+
+INSERT INTO `student_tbl` (`StudentID`, `studid`, `StudentQR`) VALUES
+(1, 1, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzcmNvZGUiOiIyMS0zNjk5OSIsImZ1bGxuYW1lIjoiVkFMREVaLCBGUllBTiBBVVJJQyBMLiIsInRpbWVzdGFtcCI6IjIwMjMtMTEtMTYgMjI6MDA6MTUiLCJ0eXBlIjoiU1RVREVOVCIsInVzZXJpZCI6IjIxLTM2OTk5In0.Zvx0BjtFexJ1dKbr295nGIUDCA9vZ44yqmdoBBw7rfc'),
+(2, 2, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzcmNvZGUiOiIyMS0zNjQ1MiIsImZ1bGxuYW1lIjoiQkFZQkFZLCBFTU1BTlVFTCBULiIsInRpbWVzdGFtcCI6IjIwMjMtMTEtMTYgMTk6MDE6MDYiLCJ0eXBlIjoiU1RVREVOVCIsInVzZXJpZCI6IjIxLTM2NDUyIn0.rXk1EvJwCKX0S1Lw9OpjZV7onA0Nzmj1VpXwPYTZgBE'),
+(3, 3, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzcmNvZGUiOiIyMS0zNjExMSIsImZ1bGxuYW1lIjoiTEFUT1JSRSwgSk9ITiBBRVJPTiBELiIsInRpbWVzdGFtcCI6IjIwMjMtMTEtMTYgMjE6NTA6MDQiLCJ0eXBlIjoiU1RVREVOVCIsInVzZXJpZCI6IjIxLTM2MTExIn0.qVMtyeO9V_qiWnM9lJe8fNT9NnZPaAYVDTFgdyAstYo');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbemployee`
 --
 
-DROP TABLE IF EXISTS `tbemployee`;
-CREATE TABLE IF NOT EXISTS `tbemployee` (
+CREATE TABLE `tbemployee` (
   `empid` int(11) NOT NULL,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `department` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbemployee`
+--
+
+INSERT INTO `tbemployee` (`empid`, `lastname`, `firstname`, `department`) VALUES
+(1, 'Reyes', 'Jennifer', 'BSIT');
 
 -- --------------------------------------------------------
 
@@ -42,20 +116,71 @@ CREATE TABLE IF NOT EXISTS `tbemployee` (
 -- Table structure for table `tb_studentinfo`
 --
 
-DROP TABLE IF EXISTS `tb_studentinfo`;
-CREATE TABLE IF NOT EXISTS `tb_studentinfo` (
+CREATE TABLE `tb_studentinfo` (
   `studid` int(11) NOT NULL,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_studentinfo`
 --
 
 INSERT INTO `tb_studentinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(1, 'parker', 'peter', 'bsit');
+(1, 'Valdez', 'Fryan Auric', 'BSIT'),
+(2, 'Baybay', 'Emmanuel', 'BSIT'),
+(3, 'Latorre', 'John Aeron', 'BSIT');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendance_tbl`
+--
+ALTER TABLE `attendance_tbl`
+  ADD PRIMARY KEY (`AttendanceID`);
+
+--
+-- Indexes for table `faculty_tbl`
+--
+ALTER TABLE `faculty_tbl`
+  ADD PRIMARY KEY (`FacultyID`);
+
+--
+-- Indexes for table `student_tbl`
+--
+ALTER TABLE `student_tbl`
+  ADD PRIMARY KEY (`StudentID`);
+
+--
+-- Indexes for table `tbemployee`
+--
+ALTER TABLE `tbemployee`
+  ADD PRIMARY KEY (`empid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendance_tbl`
+--
+ALTER TABLE `attendance_tbl`
+  MODIFY `AttendanceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `faculty_tbl`
+--
+ALTER TABLE `faculty_tbl`
+  MODIFY `FacultyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `student_tbl`
+--
+ALTER TABLE `student_tbl`
+  MODIFY `StudentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
