2550bede8c22a4490ac8e4320ea9136fc04977bc
Aleister Alinsunurin <115854134+AliHaruto@users.noreply.github.com>
2023-11-23 22:52:24 +0800

    Update db_nt3102.sql

--- db_nt3102.sql
+++ db_nt3102.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_nt3102`
 --
+CREATE DATABASE IF NOT EXISTS `db_nt3102` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `db_nt3102`;
 
 DELIMITER $$
 --
