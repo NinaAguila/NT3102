a5277edb71f2d52f0b1839e6ae992414c39e7ebb
Rica Angel Barleta <145759124+RicaAngel@users.noreply.github.com>
2023-11-24 16:57:24 +0800

    Update db_nt3102.sql

--- db_nt3102.sql
+++ db_nt3102.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 24, 2023 at 06:22 AM
+-- Generation Time: Nov 24, 2023 at 08:55 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -34,17 +34,16 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `password` varchar(60) NOT NULL,
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
-  `photo` varchar(200) NOT NULL,
-  `created_on` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `username`, `password`, `firstname`, `lastname`, `photo`, `created_on`) VALUES
-(1, 'Mekuu', 'admin123', 'Mikko', 'Igle', 'profile1.jpg', '2023-11-01');
+INSERT INTO `admin` (`id`, `username`, `password`, `firstname`, `lastname`) VALUES
+(1, 'Admin', 'admin123', 'Deku', 'Tagle'),
+(2, 'Librarian', 'librarian123', 'Ace', 'Payas');
 
 -- --------------------------------------------------------
 
@@ -63,6 +62,35 @@ CREATE TABLE IF NOT EXISTS `attendance` (
   `num_hr` double NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+CREATE TABLE IF NOT EXISTS `students` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `sr_code` varchar(15) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` text NOT NULL,
+  `course` varchar(20) NOT NULL,
+  `year_level` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `sr_code`, `firstname`, `lastname`, `email`, `course`, `year_level`) VALUES
+(1, '21-34990', 'Cyrus', 'Payas', '21-34990@gmail.com', 'BSBA', '4rd Year'),
+(2, '21-35881', 'Maiko', 'Tagle', '21-35881@gmail.com', 'BSCS', '4rd Year'),
+(3, '21-36992', 'Kyle', 'Tolentino', '21-36992@g.batstate-u.edu.ph', 'BSBA', '2nd Year'),
+(4, '21-56882', 'Paul', 'Pedrialva', '21-56882@g.batstate-u.edu.ph', 'BSCS', '3rd Year'),
+(5, '21-37224', 'Jim', 'Javier', '21-37224@g.batstate-u.edu.ph', 'BSC', '2nd Year');
 
 -- --------------------------------------------------------
 
@@ -86,27 +114,11 @@ CREATE TABLE IF NOT EXISTS `tbemployee` (
 
 DROP TABLE IF EXISTS `tb_studentinfo`;
 CREATE TABLE IF NOT EXISTS `tb_studentinfo` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `sr_code` varchar(15) NOT NULL,
-  `firstname` varchar(50) NOT NULL,
-  `lastname` varchar(50) NOT NULL,
-  `email` text NOT NULL,
-  `course` varchar(20) NOT NULL,
-  `year_level` varchar(10) NOT NULL,
-  `created_on` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_studentinfo`
---
-
-INSERT INTO `tb_studentinfo` (`id`, `sr_code`, `firstname`, `lastname`, `email`, `course`, `year_level`, `created_on`) VALUES
-(1, '21-34990', 'Cyrus', 'Payas', '21-34990@gmail.com', 'BSBA', '4rd Year', '2010-10-23'),
-(2, '21-35881', 'Maiko', 'Tagle', '21-35881@gmail.com', 'BSCS', '4rd Year', '2022-02-09'),
-(3, '21-36992', 'Kyle', 'Tolentino', '21-36992@g.batstate-u.edu.ph', 'BSBA', '2nd Year', '2008-11-23'),
-(4, '21-56882', 'Paul', 'Pedrialva', '21-56882@g.batstate-u.edu.ph', 'BSCS', '3rd Year', '2023-10-23'),
-(5, '21-37224', 'Jim', 'Javier', '21-37224@g.batstate-u.edu.ph', 'BSC', '2nd Year', '2026-10-23');
+  `studid` int NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `course` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
