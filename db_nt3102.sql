0b063d150cf6cf9c7a832a905fcb5ce61bbf865d
Enebres <143394767+Elbert-Nebres@users.noreply.github.com>
2023-11-24 15:01:39 +0800

    Add files via upload

--- db_nt3102.sql
+++ db_nt3102.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 20, 2023 at 01:30 AM
+-- Generation Time: Nov 24, 2023 at 07:00 AM
 -- Server version: 8.0.34
 -- PHP Version: 8.0.26
 
@@ -37,6 +37,22 @@ CREATE TABLE IF NOT EXISTS `admin` (
   PRIMARY KEY (`adminID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`adminID`, `adminFN`, `adminLN`, `adminEmailAddress`, `adminPassword`) VALUES
+(1, 'John', 'Doe', 'john.doe@email.com', 'password1'),
+(2, 'Jane', 'Smith', 'jane.smith@email.com', 'password2'),
+(3, 'Bob', 'Johnson', 'bob.johnson@email.com', 'password3'),
+(4, 'Alice', 'Williams', 'alice.williams@email.com', 'password4'),
+(5, 'Charlie', 'Brown', 'charlie.brown@email.com', 'password5'),
+(6, 'Eva', 'Davis', 'eva.davis@email.com', 'password6'),
+(7, 'Frank', 'Taylor', 'frank.taylor@email.com', 'password7'),
+(8, 'Grace', 'Moore', 'grace.moore@email.com', 'password8'),
+(9, 'Henry', 'Clark', 'henry.clark@email.com', 'password9'),
+(10, 'Ivy', 'Anderson', 'ivy.anderson@email.com', 'password10');
+
 -- --------------------------------------------------------
 
 --
@@ -46,10 +62,10 @@ CREATE TABLE IF NOT EXISTS `admin` (
 DROP TABLE IF EXISTS `author`;
 CREATE TABLE IF NOT EXISTS `author` (
   `authorID` int NOT NULL AUTO_INCREMENT,
-  `authorFn` text COLLATE utf8mb4_general_ci,
-  `authorLn` text COLLATE utf8mb4_general_ci,
+  `authorFn` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `authorLn` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`authorID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `author`
@@ -58,7 +74,12 @@ CREATE TABLE IF NOT EXISTS `author` (
 INSERT INTO `author` (`authorID`, `authorFn`, `authorLn`) VALUES
 (2, 'Aia', 'Amare'),
 (3, 'Fulgur', 'Ovid'),
-(4, 'John', 'Doe');
+(4, 'John', 'Doe'),
+(5, 'Milie', 'Parfait'),
+(6, 'Johns', 'Doe'),
+(7, 'John', 'Doo'),
+(8, 'millie', 'Parfait'),
+(9, 'Enna', 'alouette');
 
 -- --------------------------------------------------------
 
@@ -71,10 +92,10 @@ CREATE TABLE IF NOT EXISTS `book` (
   `bookID` int NOT NULL AUTO_INCREMENT,
   `authorID` int DEFAULT NULL,
   `publisherID` int DEFAULT NULL,
-  `bookTitle` text COLLATE utf8mb4_general_ci,
-  `ISBN` text COLLATE utf8mb4_general_ci,
-  `description` longtext COLLATE utf8mb4_general_ci,
-  `genre` text COLLATE utf8mb4_general_ci,
+  `bookTitle` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `ISBN` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `description` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `genre` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `publishDate` date DEFAULT NULL,
   `quantity` int DEFAULT NULL,
   `goodbook` int NOT NULL,
@@ -82,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `book` (
   PRIMARY KEY (`bookID`),
   KEY `authorID` (`authorID`),
   KEY `publisherID` (`publisherID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `book`
@@ -92,10 +113,8 @@ INSERT INTO `book` (`bookID`, `authorID`, `publisherID`, `bookTitle`, `ISBN`, `d
 (1, 2, 1, 'Dork', '0-2029-2791-1', 'ILUNA Institute of the Mystics 2nd Year.\r\nAn angelic maiden who descended from Heaven to observe mankind.\r\nHer gentle appearance hides a rather mischievous disposition 1', 'Non-Fiction', '2023-01-01', 10, 5, 5),
 (2, 3, 2, 'Legatus 404', '0-1260-6478-4', 'A Cyborg from the future with unparalleled strength.\r\nHis body is partly cold steel, and who is to say if he has a heart', 'Fiction', '2023-02-02', 20, 10, 10),
 (3, 4, 3, 'though Doe', '0-4319-1393-5', 'A typical AI generated name', 'Comedy', '2023-03-03', 30, 20, 10),
-(4, NULL, NULL, 'Flat is justice', '2312314', 'witch', 'Fantasy', '2023-11-23', 1312, 1, 1),
-(5, NULL, NULL, 'Flat is justice', '2312314', 'witch', 'Fantasy', '2023-11-10', 2, 3, 4),
-(6, 2, NULL, 'Flat is justice', '2312314', 'dawa', 'Fiction', '2023-11-02', 4, 2, 2),
-(7, 2, NULL, 'Flat is justice', '2312314', 'dawa', 'Fiction', '2023-11-02', 4, 2, 2);
+(6, 6, 1, 'Flat is justice', '32141', 'A monster cat creeping through hell. A bit aggressive, but maybe she just needs some attention.[1]', 'Fantasy', '2023-11-21', 6, 2, 4),
+(8, 8, 3, 'Song Bird', '2432', 'A blue bird fluttering in the heavens. She sings a love song for the souls of the living', 'Fantasy', '2023-11-01', 10, 4, 6);
 
 -- --------------------------------------------------------
 
@@ -106,12 +125,27 @@ INSERT INTO `book` (`bookID`, `authorID`, `publisherID`, `bookTitle`, `ISBN`, `d
 DROP TABLE IF EXISTS `librarian`;
 CREATE TABLE IF NOT EXISTS `librarian` (
   `libID` int NOT NULL AUTO_INCREMENT,
-  `libFN` text,
-  `libLN` text,
-  `librarianEmail` text,
-  `librarianPassword` text,
+  `libFN` varchar(25) NOT NULL,
+  `libLN` varchar(25) NOT NULL,
+  `librarianPassword` varchar(25) NOT NULL,
   PRIMARY KEY (`libID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `librarian`
+--
+
+INSERT INTO `librarian` (`libID`, `libFN`, `libLN`, `librarianPassword`) VALUES
+(101, 'Sarah', 'Miller', 'librarianpass1'),
+(102, 'David', 'Jones', 'librarianpass2'),
+(103, 'Emily', 'White', 'librarianpass3'),
+(104, 'Mark', 'Johnson', 'librarianpass4'),
+(105, 'Linda', 'Taylor', 'librarianpass5'),
+(106, 'Brian', 'Smith', 'librarianpass6'),
+(107, 'Catherine', 'Brown', 'librarianpass7'),
+(108, 'Daniel', 'Clark', 'librarianpass8'),
+(109, 'Olivia', 'Davis', 'librarianpass9'),
+(110, 'Peter', 'Wilson', 'librarianpass10');
 
 -- --------------------------------------------------------
 
@@ -122,10 +156,10 @@ CREATE TABLE IF NOT EXISTS `librarian` (
 DROP TABLE IF EXISTS `publisher`;
 CREATE TABLE IF NOT EXISTS `publisher` (
   `publisherID` int NOT NULL AUTO_INCREMENT,
-  `publisherName` text COLLATE utf8mb4_general_ci,
-  `publisherAddress` text COLLATE utf8mb4_general_ci,
+  `publisherName` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `publisherAddress` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`publisherID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `publisher`
@@ -134,7 +168,14 @@ CREATE TABLE IF NOT EXISTS `publisher` (
 INSERT INTO `publisher` (`publisherID`, `publisherName`, `publisherAddress`) VALUES
 (1, 'ABC Production', 'Cuenca, Batangas'),
 (2, 'Victory', 'Lipa City, Batangas'),
-(3, 'Modico', 'Biñan, Laguna');
+(3, 'Modico', 'Biñan, Laguna'),
+(4, 'City Publishers', '321 Maple St, City4, Country'),
+(5, 'Metro Print House', '555 Pine St, City5, Country'),
+(6, 'Sunny Publications', '777 Cedar St, City6, Country'),
+(7, 'Valley Books', '999 Birch St, City7, Country'),
+(8, 'Summit Press', '234 Spruce St, City8, Country'),
+(9, 'Harbor Publishing', '876 Fir St, City9, Country'),
+(10, 'Meadow Books Inc', '543 Redwood St, City10, Country');
 
 -- --------------------------------------------------------
 
@@ -147,8 +188,18 @@ CREATE TABLE IF NOT EXISTS `tbemployee` (
   `empid` int NOT NULL,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
-  `department` varchar(20) NOT NULL
+  `department` varchar(20) NOT NULL,
+  PRIMARY KEY (`empid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbemployee`
+--
+
+INSERT INTO `tbemployee` (`empid`, `lastname`, `firstname`, `department`) VALUES
+(1001, 'Dela Cruz', 'Juan', 'BSIT'),
+(1002, 'Santos', 'Maria', 'BSCS'),
+(1003, 'Reyes', 'Pedro', 'BSIS');
 
 -- --------------------------------------------------------
 
@@ -161,8 +212,18 @@ CREATE TABLE IF NOT EXISTS `tb_studentinfo` (
   `studid` int NOT NULL,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
-  `course` varchar(20) NOT NULL
+  `course` varchar(20) NOT NULL,
+  PRIMARY KEY (`studid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tb_studentinfo`
+--
+
+INSERT INTO `tb_studentinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
+(1001, 'Dela Cruz', 'Juan', 'BSIT'),
+(1002, 'Santos', 'Maria', 'BSCS'),
+(1003, 'Reyes', 'Pedro', 'BSIS');
 
 -- --------------------------------------------------------
 
@@ -173,12 +234,76 @@ CREATE TABLE IF NOT EXISTS `tb_studentinfo` (
 DROP TABLE IF EXISTS `updatelog`;
 CREATE TABLE IF NOT EXISTS `updatelog` (
   `updateID` int NOT NULL AUTO_INCREMENT,
-  `action` text COLLATE utf8mb4_general_ci,
-  `entityType` text COLLATE utf8mb4_general_ci,
+  `action` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `entityType` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `entityID` int DEFAULT NULL,
   `timestamp` datetime DEFAULT NULL,
   PRIMARY KEY (`updateID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `updatelog`
+--
+
+INSERT INTO `updatelog` (`updateID`, `action`, `entityType`, `entityID`, `timestamp`) VALUES
+(1, 'Delete', 'Staff', 1, '2023-11-24 13:52:26'),
+(2, 'Delete', 'User', 2, '2023-11-24 13:57:34'),
+(3, 'ADD', 'Staff', 3, '2023-11-24 13:57:34'),
+(4, 'Delete', 'User', 4, '2023-11-24 13:57:34'),
+(5, 'ADD', 'User', 5, '2023-11-24 13:57:34'),
+(6, 'ADD', 'Staff', 6, '2023-11-24 13:57:34'),
+(7, 'Delete', 'User', 7, '2023-11-24 13:57:34'),
+(8, 'Delete', 'Staff', 8, '2023-11-24 13:57:34'),
+(9, 'Add', 'Staff', 9, '2023-11-24 13:57:34'),
+(10, 'Delete', 'Staff', 10, '2023-11-24 13:57:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usere`
+--
+
+DROP TABLE IF EXISTS `usere`;
+CREATE TABLE IF NOT EXISTS `usere` (
+  `UserEID` int NOT NULL AUTO_INCREMENT,
+  `empid` int NOT NULL,
+  `UserE_password` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`UserEID`),
+  KEY `fk_empid` (`empid`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `usere`
+--
+
+INSERT INTO `usere` (`UserEID`, `empid`, `UserE_password`) VALUES
+(4, 1001, 'Emp'),
+(5, 1002, 'EMp'),
+(6, 1003, 'emP');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `UserSID` int NOT NULL AUTO_INCREMENT,
+  `studid` int NOT NULL,
+  `UserS_password` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`UserSID`),
+  KEY `fk_studid` (`studid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserSID`, `studid`, `UserS_password`) VALUES
+(1, 1001, 'Student'),
+(2, 1002, 'Student'),
+(3, 1003, 'Student');
 
 --
 -- Constraints for dumped tables
@@ -190,6 +315,18 @@ CREATE TABLE IF NOT EXISTS `updatelog` (
 ALTER TABLE `book`
   ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`authorID`) REFERENCES `author` (`authorID`),
   ADD CONSTRAINT `book_ibfk_2` FOREIGN KEY (`publisherID`) REFERENCES `publisher` (`publisherID`);
+
+--
+-- Constraints for table `usere`
+--
+ALTER TABLE `usere`
+  ADD CONSTRAINT `fk_empid` FOREIGN KEY (`empid`) REFERENCES `tbemployee` (`empid`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_studid` FOREIGN KEY (`studid`) REFERENCES `tb_studentinfo` (`studid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
